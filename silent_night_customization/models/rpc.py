121999ed437726beb4fc65fdc6136db9e0a16f8b
97jaw <jawkhan111@gmail.com>
2023-01-16 13:04:42 +0500

    Crons updated code for Line and SO ----- v1

--- silent_night_customization/models/rpc.py
+++ silent_night_customization/models/rpc.py
@@ -50,96 +50,104 @@ class Account(models.Model):
              ('state', '=', 'sale')], order='id asc', limit=40)
         counter = 0
         for sl_id in sale_orders:
-            if sl_id not in [38520, 38521]:
-                so = odoo.env['sale.order'].browse(sl_id)
-                # print(so.updated_in_shopify)
-                sale_order_id = self.env['sale.order'].create({
-                    'x_id': so.id,
-                    'name': so.name,
-                    'partner_id': self.get_partner_id(so),
-                    'makan_number': so.dest_makani_number,
-                    'delivery_date': so.pref_date,
-                    'beneficiary_delivery_date': so.beneficiary_delivery_date,
-                    'analytic_account': self.get_acc_id(so),
-                    'beneficiary': self.get_benef_id(so),
-                    'client_order_ref': so.client_order_ref,
-                    'date_order': so.date_order,
-                    'pricelist_id': self.get_pricelist_id(so),
-                    'payment_term_id': self.get_payment_term_id(so),
-                    'warehouse_id': self.get_warehouse_id(so),
-                    'credit_limit': so.credit_limit,
-                    'earliest_due_date': so.earliest_due_date,
-                    'state': so.state,
-                    'updated_in_shopify': so.updated_in_shopify
-                })
-                counter += 1
-                print("==========", counter)
+            # if sl_id not in [38520, 38521]:
+            so = odoo.execute('sale.order', 'read', [sl_id],
+                              ['id', 'name', 'partner_id', 'dest_makani_number', 'pref_date',
+                               'beneficiary_delivery_date', 'analytic_account_id', 'partner_id', 'client_order_ref',
+                               'date_order', 'pricelist_id', 'payment_term_id', 'warehouse_id', 'credit_limit',
+                               'earliest_due_date', 'state'])
+            # so = odoo.env['sale.order'].browse(sl_id)
+            # print(so.updated_in_shopify)
+            sale_order_id = self.env['sale.order'].create({
+                'x_id': so[0]['id'],
+                'name': so[0]['name'],
+                'partner_id': self.get_partner_id(so[0]),
+                'makan_number': so[0]['dest_makani_number'],
+                'delivery_date': so[0]['pref_date'],
+                'beneficiary_delivery_date': so[0]['beneficiary_delivery_date'],
+                'analytic_account': self.get_acc_id(so[0]),
+                'beneficiary': self.get_benef_id(so[0]),
+                'client_order_ref': so[0]['client_order_ref'],
+                'date_order': so[0]['date_order'],
+                'pricelist_id': self.get_pricelist_id(so[0]),
+                'payment_term_id': self.get_payment_term_id(so[0]),
+                'warehouse_id': self.get_warehouse_id(so[0]),
+                'credit_limit': so[0]['credit_limit'],
+                'earliest_due_date': so[0]['earliest_due_date'],
+                'state': so[0]['state'],
+            })
+            counter += 1
+            print("==========", counter)
 
     def get_partner_id(self, aa):
-        if aa.partner_id:
-            partner = self.env['res.partner'].search([('name', '=', aa.partner_id.name)], limit=1)
+        if aa['partner_id']:
+            if "," in aa['partner_id'][1]:
+                name = aa['partner_id'][1].split(',')[1].strip()
+            else:
+                name = aa['partner_id'][1].strip()
+            partner = self.env['res.partner'].search([('name', '=', name)],
+                                                     limit=1)
             if partner:
                 return partner.id
             else:
                 x_partner = self.env['res.partner'].create({
 
-                    'name': aa.name
+                    'name': name
                 })
                 return x_partner.id
         else:
             return False
-        print("CUSTOMER")
 
     def get_benef_id(self, aa):
-        if aa.partner_id:
-            partner = self.env['res.partner'].search([('name', '=', aa.partner_id_beneficiary.name)], limit=1)
+        if aa['partner_id']:
+            if "," in aa['partner_id'][1]:
+                name = aa['partner_id'][1].split(',')[1].strip()
+            else:
+                name = aa['partner_id'][1].strip()
+            partner = self.env['res.partner'].search([('name', '=', name)], limit=1)
             if partner:
                 return partner.id
             else:
                 x_partner = self.env['res.partner'].create({
 
-                    'name': aa.partner_id_beneficiary.name
+                    'name': name
                 })
                 return x_partner.id
         else:
             return False
-        print("BENEFICIARY")
 
     def get_warehouse_id(self, so):
-        if so.warehouse_id:
-            pt = self.env['stock.warehouse'].search([('name', '=', so.warehouse_id.name)], limit=1)
+        if so['warehouse_id']:
+            pt = self.env['stock.warehouse'].search([('name', '=', so['warehouse_id'][1])], limit=1)
             if pt:
                 return pt.id
         else:
             return False
-        print("WAREHOUSE")
 
     def get_payment_term_id(self, so):
-        if so.payment_term_id:
-            pt = self.env['account.payment.term'].search([('name', '=', so.payment_term_id.name)], limit=1)
+        if so['payment_term_id']:
+            pt = self.env['account.payment.term'].search([('name', '=', so['payment_term_id'][1])], limit=1)
             if pt:
                 return pt.id
         else:
             return False
-        print("PAYMENT TERM")
 
     def get_pricelist_id(self, so):
-        if so.pricelist_id:
-            pricelst = self.env['product.pricelist'].search([('x_id', '=', so.pricelist_id.id)])
+        if so['pricelist_id']:
+            pricelst = self.env['product.pricelist'].search([('x_id', '=', so['pricelist_id'][0])])
             if pricelst:
                 return pricelst.id
-        else:
-            return False
-        print("PRICELIST")
+            else:
+                return 19
 
     def get_acc_id(self, so):
-        if so.analytic_account_id:
-            acc = self.env['account.analytic.account'].search([('x_id', '=', so.analytic_account_id.id)])
+        if so['analytic_account_id']:
+            acc = self.env['account.analytic.account'].search([('x_id', '=', so['analytic_account_id'][0])])
             if acc:
                 return acc.id
         else:
             return False
-        print("A ACCOUNT")
+
         # for pid in product_ids:
         #     product_data = odoo.execute('product.template', 'read', [pid])
         # print(user_data)
@@ -173,56 +181,62 @@ class Account(models.Model):
         # Current user
         user = odoo.env.user
 
-        sale_orders = self.env['sale.order'].search([('x_is_updated', '=', False)], limit=10)
+        sale_orders = self.env['sale.order'].search([('x_is_updated', '=', False)],limit=40)
 
         for order_id in sale_orders:
             x_sale_line = odoo.env['sale.order.line'].search([('id', '=', int(order_id.x_id))])
             for x_line_id in x_sale_line:
-                x_line = odoo.env['sale.order.line'].browse(x_line_id)
+                x_line = odoo.execute('sale.order.line', 'read', [x_line_id],
+                                      ['name', 'product_id', 'product_uom_qty', 'qty_delivered', 'qty_invoiced',
+                                       'product_uom', 'price_unit', 'order_id', 'tax_id', 'x_pid'])
+                # x_line = odoo.env['sale.order.line'].browse(x_line_id)
+                print("X-SO-ID=====>>>>>>>", order_id.x_id)
+                if x_line[0]['product_id']:
+                    sale_order_line = self.env['sale.order.line'].create({
 
-                sale_order_line = self.env['sale.order.line'].create({
+                        'name': x_line[0]['name'],
+                        'product_id': self.get_product_id(x_line[0], odoo),
+                        'product_uom_qty': x_line[0]['product_uom_qty'],
+                        'qty_delivered': x_line[0]['qty_delivered'],
+                        'qty_invoiced': x_line[0]['qty_invoiced'],
+                        'product_uom': self.get_product_uom_id(x_line[0]),
+                        'price_unit': x_line[0]['price_unit'],
+                        'order_id': order_id.id,
+                        'tax_id': self.get_tax_id(x_line[0], odoo),
+                    })
+                order_id.x_is_updated = True
+                order_id.x_ref = x_line[0]['order_id'][1]
 
-                    'name': x_line.name,
-                    'product_id': self.get_product_id(x_line, odoo),
-                    'product_uom_qty': x_line.product_uom_qty,
-                    'qty_delivered': x_line.qty_delivered,
-                    'qty_invoiced': x_line.qty_invoiced,
-                    'product_uom': self.get_product_uom_id(x_line),
-                    'price_unit': x_line.price_unit,
-                    'order_id': order_id.id,
-                    'tax_id': self.get_tax_id(x_line),
-                })
-            order_id.x_is_updated = True
-            order_id.x_ref = x_line.order_id.name
-            print()
-
-    def get_tax_id(self, x_line):
+    def get_tax_id(self, x_line, odoo):
         list = []
-        for tax_id in x_line.tax_id:
-            tax = self.env['account.tax'].search([('name', '=', tax_id.name)])
+        for tax_id in x_line['tax_id']:
+            x_tax = odoo.execute('account.tax', 'read', [tax_id], ['name'])
+            tax = self.env['account.tax'].search([('name', '=', x_tax[0]['name'])])
             if tax:
                 list.append(tax.ids)
         return list
 
     def get_product_uom_id(self, x_line):
-        uom = self.env['uom.uom'].search([('name', '=', x_line.product_uom.name)])
+        uom = self.env['uom.uom'].search([('name', '=', x_line['product_uom'][1])])
         if uom:
             return uom.id
         else:
             return False
 
     def get_product_id(self, x_line, odoo):
-        x_product = odoo.execute('product.product', 'read', [int(x_line.x_pid)], ['name', 'list_price', 'standard_price'])
-        product = self.env['product.product'].search([('name', '=', x_product[0]['name'])],limit=1)
-        if product:
-            product.update({'x_id': x_product[0]['id']})
-            return product.id
-        else:
-            x_product = self.env['product.product'].create({'x_id': x_product[0]['id'],
-                                                            'name': x_product[0]['name'],
-                                                            'sale_ok': True,
-                                                            'categ_id' : 530,
-                                                            'list_price': x_product[0]['list_price'],
-                                                            'standard_price': x_product[0]['standard_price']
-                                                            })
-            return x_product.id
+        if x_line['product_id']:
+            x_product = odoo.execute('product.product', 'read', [x_line['x_pid']],
+                                     ['name', 'list_price', 'standard_price'])
+            product = self.env['product.product'].search([('name', '=', x_product[0]['name'])], limit=1)
+            if product:
+                product.update({'x_id': x_product[0]['id']})
+                return product.id
+            else:
+                x_product = self.env['product.product'].create({'x_id': x_product[0]['id'],
+                                                                'name': x_product[0]['name'] or "test",
+                                                                'sale_ok': True,
+                                                                'categ_id': 530,
+                                                                'list_price': x_product[0]['list_price'],
+                                                                'standard_price': x_product[0]['standard_price']
+                                                                })
+                return x_product.id
